7620f6b356d03b5903be9f3e5a4bac9a1701c362
Satsuki Akiba <anosatsuk124@gmail.com>
2022-12-19 17:32:55 +0900

    Replace preact with react and add MUI

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,27 @@
-import './app.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-export function App<FC>() {
-    return <div class="container"></div>;
-}
+const App = (): React.ReactElement => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                    <MenuIcon />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default App;
