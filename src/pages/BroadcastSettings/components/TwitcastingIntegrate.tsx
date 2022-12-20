7893e564d882373c3b8da6a661f7bebfcdcce7a7
Satsuki Akiba <anosatsuk124@gmail.com>
2022-12-20 14:38:14 +0900

    Revert "Revert "Update""
    This reverts commit d300cba759c84ab172d656a478d6c56082083918.

--- src/pages/BroadcastSettings/components/TwitcastingIntegrate.tsx
+++ src/pages/BroadcastSettings/components/TwitcastingIntegrate.tsx
@@ -3,8 +3,53 @@ import { useTranslation } from 'react-i18next';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 import { tryAuthorization } from '../../../comments/twitcasting/client';
+
+const LoginInfo: React.FC = () => {
+    const { t } = useTranslation();
+
+    const [isAuthorized, setIsAuthorized] = React.useState(false);
+
+    return (
+        <Box>
+            <Card>
+                <CardContent>
+                    <Typography
+                        align="center"
+                        variant="h6"
+                        component="div"
+                        color={isAuthorized ? 'primary' : 'textSecondary'}
+                    >
+                        {isAuthorized
+                            ? t('broadcast-settings.twitcasting.authorized')
+                            : t('broadcast-settings.twitcasting.unauthorized')}
+                    </Typography>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        onClick={async () => {
+                            // await tryAuthorization(clientId, clientSecret);
+                        }}
+                    >
+                        {!isAuthorized
+                            ? t('broadcast-settings.twitcasting.login')
+                            : t('broadcast-settings.twitcasting.logout')}
+                        {isAuthorized ? <LogoutIcon /> : <LoginIcon />}
+                    </Button>
+                </CardContent>
+            </Card>
+        </Box>
+    );
+};
 
 const TwitcastingIntegrate: React.FC = () => {
     const { t } = useTranslation();
@@ -25,23 +70,20 @@ const TwitcastingIntegrate: React.FC = () => {
 
     return (
         <Box>
-            <TextField
-                label={t('broadcast-settings.twitcasting.client-id')}
-                onChange={handleChangeClientId}
-            />
-            <TextField
-                label={t('broadcast-settings.twitcasting.client-secret')}
-                onChange={handleChangeClientSecret}
-            />
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={async () => {
-                    await tryAuthorization(clientId, clientSecret);
-                }}
-            >
-                {t('broadcast-settings.twitcasting.authorize')}
-            </Button>
+            <LoginInfo />
+            <FormHelperText>
+                {t('broadcast-settings.twitcasting.label')}
+            </FormHelperText>
+            <FormControl fullWidth>
+                <TextField
+                    label={t('broadcast-settings.twitcasting.client-id')}
+                    onChange={handleChangeClientId}
+                />
+                <TextField
+                    label={t('broadcast-settings.twitcasting.client-secret')}
+                    onChange={handleChangeClientSecret}
+                />
+            </FormControl>
         </Box>
     );
 };
